202f7cc7b8e6268805d21b6a562a32ff322b58f8
Darcy <huqy1021_cd@outlook.com>
2024-08-06 17:53:07 +1000

    navbar init

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,21 +1,15 @@
 // frontend/pages/index.tsx
-import Navbar from './navbar';
-import Hero from './hero';
-import Projects from './projects';
-import Skills from './skills';
-import About from './about';
-import Contact from './contact';
-import Resume from './resume'
+import Navbar from "./navbar";
+import Hero from "./hero";
+import Projects from "./projects";
+import Skills from "./skills";
+import About from "./about";
+import Contact from "./contact";
+import Resume from "./resume";
 
 const Home = () => (
   <>
     <Navbar />
-    <Hero />
-    <Projects />
-    <Skills />
-    <About />
-    <Contact />
-    <Resume />
   </>
 );
 
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      borderRadius: {
+        "50": "50px",
+        "10": "10px",
+      },
     },
   },
   plugins: [],
