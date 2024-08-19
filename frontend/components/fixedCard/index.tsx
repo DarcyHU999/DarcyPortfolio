82d7680ab179ef4ad998d5850c6db7d8fdeadfd7
DarcyHU999 <huqy1021_cd@outlook.com>
2024-08-19 23:17:40 +1000

    almost finished

--- frontend/components/fixedCard/index.tsx
+++ frontend/components/fixedCard/index.tsx
@@ -16,22 +16,21 @@ const information={
 
 const FixedCard=()=>{
     return (
-        <div className="bg-white w-2/3 xl:w-1/3 mr-10 mb-5 mt-60 pt-5 rounded-50 relative">
-            <div className="relative bottom-60 flex flex-col justify-center items-center">
-                <Image className="rounded-10" width={350} height={350} src={information.photo} alt={"Darcy Photo"}/>
-                    <div className="flex flex-row my-5 text-5xl">
+        <div className="relative bg-white w-full mb-5 pt-5 rounded-10">
+            <div className="relative flex flex-col justify-center items-center mb-5">
+                <Image className="rounded-10 w-1/3 h-1/3" width={200} height={200} src={information.photo} alt={"Darcy Photo"}/>
+                    <div className="flex flex-row my-5 text-3xl">
                         <div className="mr-2">{information.firstName}</div>
                         <div className="font-bold">{information.lastName}                        
-                            <a href={information.linkedin} target="_blank" rel="noreferrer"><LinkedIn className="absolute text-blue-500 ml-1 w-8 h-8"/></a>
+                            <a href={information.linkedin} target="_blank" rel="noreferrer"><LinkedIn className="absolute text-blue-500 ml-1 w-6 h-6"/></a>
                         </div>
                     </div>
-                <section className="text-3xl w-80 h-12 bg-gray-200 rounded-50 flex flex-row items-center justify-center">{information.position}</section>
+                <section className="text-xl w-2/3 h-12 bg-gray-200 rounded-50 flex flex-row items-center justify-center">{information.position}</section>
             </div>
-            
-            <div className="relative bg-gray-200 flex flex-col -mt-44 h-auto rounded-b-50 py-3 px-10 items-start">
-                <p className="relative xl:left-0 text-3xl w-auto"><LocationOnOutlinedIcon className="w-12 h-12 my-2 mr-2 text-blue-500"/>{information.address}</p>
-                <p className="relative xl:left-0 text-3xl w-auto"><MailOutlineOutlinedIcon className="w-12 h-12 my-2 mr-2 text-blue-500"/>{information.email}</p>
-                <p className="relative xl:left-0 text-3xl w-auto"><PhoneIphoneOutlinedIcon className="w-12 h-12 my-2 mr-2 text-blue-500"/>{information.phone}</p>
+            <div className="relative flex flex-col bg-gray-200  h-auto rounded-b-10 py-3 px-5 items-start">
+                <p className="relative text-xl w-auto"><LocationOnOutlinedIcon className="w-6 h-6 my-2 mr-2 text-blue-500"/>{information.address}</p>
+                <p className="relative text-xl w-auto"><MailOutlineOutlinedIcon className="w-6 h-6 my-2 mr-2 text-blue-500"/>{information.email}</p>
+                <p className="relative text-xl w-auto"><PhoneIphoneOutlinedIcon className="w-6 h-6 my-2 mr-2 text-blue-500"/>{information.phone}</p>
             </div>
         </div>
     )
--- frontend/components/projCard/projCard.tsx
+++ frontend/components/projCard/projCard.tsx
@@ -27,18 +27,17 @@ const ProjCard:React.FC<ProjcardProps> = ({projects})=>{
                 return(
                 <div key={`projcard-${index}`} className="w-1/2 my-1 py-5 px-5">
                     <div className="bg-white shadow rounded-10 py-5 flex flex-row">
-                    <section className="flex flex-col">
-                        <Image className="px-5 rounded-10" width={400} height={400} src={item.imgSrc} alt={item.imgAlt} />
-                        
-                    </section>
-                    <section className="px-5 flex flex-col">
-                        <div className="flex flex-row items-center ">
-                            <h3 className="font-bold">{item.projName}</h3>
-                            {item.website?(<a href={item.website} rel="noreferrer" target="_blank"><PublicIcon className="text-blue-500 ml-2"/></a>):undefined}
-                        </div>
-                        {item.note?<p className={`italic text-xs my-3`}><label className="font-bold">Note: </label>{item.note}</p>:undefined}
-                        <p>{item.description}</p>
-                    </section>
+                        <section className="flex flex-col w-1/3 h-1/3">
+                            <Image className="px-5 rounded-10 " width={400} height={400} src={item.imgSrc} alt={item.imgAlt} />
+                        </section>
+                        <section className="px-5 flex flex-col w-2/3">
+                            <div className="flex flex-row items-center ">
+                                <h3 className="font-bold">{item.projName}</h3>
+                                {item.website?(<a href={item.website} rel="noreferrer" target="_blank"><PublicIcon className="text-blue-500 ml-2"/></a>):undefined}
+                            </div>
+                            {item.note?<p className={`italic text-xs my-3`}><label className="font-bold">Note: </label>{item.note}</p>:undefined}
+                            <p>{item.description}</p>
+                        </section>
                     </div>
                 </div>
                 )
